e9cb33428187a91454992eb3fda9d4c82d7378e9
Leo C. Stein <leo.stein@gmail.com>
2022-02-01 21:05:37 -0600

    Add the def's and newcommand's from leops

--- auctex/style/leops.el
+++ auctex/style/leops.el
@@ -5,6 +5,21 @@
 (require 'tex)
 (require 'latex)
 
+(defvar LaTeX-leops-package-options-list
+  '(;; Uni and course info
+    ("schoolname")
+    ("deptname")
+    ("coursename")
+    ("coursenum")
+    ("termyear")
+    ("instructor")
+    ;; Problem set info
+    ("psnum")
+    ("psdue")
+    ;; Show solutions or not
+    ("solutions" ("true" "false")))
+  "Package options for the leops package.")
+
 (TeX-add-style-hook
  "leops"
  (function
@@ -13,6 +28,21 @@
                          "amsfonts" "amsthm" "xcolor" "tabularx"
                          "fontenc" "inputenc" "enumitem" "fancyhdr"
                          "hyperref" "graphicx" "bm" "verbatim"
-                         "caption"))))
+                         "caption")
+    (TeX-add-symbols
+     '("pssetup" (TeX-arg-key-val LaTeX-leops-package-options-list))
+     '("showsolutions" 0)
+     '("hidesolutions" 0)
+     '("makeheader" 0)
+     '("bs" t)
+     '("pd" 0)
+     '("nn" 0)
+     '("cd" 0)
+     '("rcurs" 0)
+     '("brcurs" 0)
+     '("hrcurs" 0)
+     '("diag" 0)
+     '("itemrange" t))
+    )))
 
 ;;; leops.el ends here.
