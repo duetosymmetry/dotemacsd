6f06580a32df88e369638ee4512e689e23b5de1f
Leo C. Stein <leo.stein@gmail.com>
2025-09-18 16:01:33 -0500

    Use my own MenloPatched font as default

--- init.el
+++ init.el
@@ -149,14 +149,12 @@
  '(warning-suppress-types '((comp)))
  '(wc-modeline-format "WC[%W%w=%twW; %C%c=%tcC]")
  '(which-key-mode t))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Hack"))))
-;;  '(line-number ((t (:background "#383838" :foreground "#6F6F6F" :family "Hack"))))
-;;  '(tab-bar ((t (:height 1.1 :foreground "black" :background "grey85" :inherit variable-pitch)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "MenloPatched" :foundry "nil" :slant normal :weight regular :height 120 :width normal)))))
 
 ;; ;; Use Menlo as a fallback in fontset-startup
 ;; ;; before resorting to fontset-default.
